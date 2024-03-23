217682e07cce5b8bd5b91c8fd9fd25b49ef18f72
walloomonero <wallaskhoza@gmail.com>
2024-03-23 04:30:28 +0200

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 #!/usr/bin/pup
-#Puppet Manifest: Install Flask version 2.1.0 using pip3
+
+#Puppet Manifest: Install Flask 2.1.0 and Werkzeug 2.1.1 using pip3
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
