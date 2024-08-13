6257068c4155e8ed0e32f3ec684b5476fd37b799
walloomonero <42607637+walloomonero@users.noreply.github.com>
2024-08-13 08:50:10 +0200

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,6 +0,0 @@
-# fixes nginx to accept and serve more requests
-
-exec {'modify max open files limit setting':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
-  path    => '/usr/local/bin/:/bin/'
-}
